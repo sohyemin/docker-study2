29a45b5f69ac0014f7668249945761e61734de45
sohyemin <sohyem.dev@gmail.com>
2024-02-03 18:00:53 +0900

    변경

---
+++ src/main/java/dockerStudy2/docker2/Docker2Application.java
@@ -0,0 +1,13 @@
+package dockerStudy2.docker2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Docker2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Docker2Application.class, args);
+	}
+
+}
---
+++ src/test/java/dockerStudy2/docker2/Docker2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dockerStudy2.docker2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Docker2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
